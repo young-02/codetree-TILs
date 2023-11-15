82ea3d0a73576ee3f731e67e0e3bc4d6c5617af1
young <75434044+young-02@users.noreply.github.com>
2023-11-15 15:31:28 +0900

    Title: 길이 단위 변환하기, Time: 250ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 231115/길이 단위 변환하기/change-length-unit.js
@@ -0,0 +1,8 @@
+let fit = 30.48
+let mi = 160934
+let totalFit = fit * 9.2
+let totalMi = mi * 1.3
+
+
+console.log(`9.2ft = ${totalFit.toFixed(1)}cm`)
+console.log(`1.3mi = ${totalMi.toFixed(1)}cm`)
