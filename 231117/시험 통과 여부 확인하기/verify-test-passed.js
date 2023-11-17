3b5c1a57924802d73f3a1f29a6a8f9fe273f73e0
young <75434044+young-02@users.noreply.github.com>
2023-11-17 12:30:03 +0900

    Title: 시험 통과 여부 확인하기, Time: 227ms, Memory: 13MB, Status: Passed - CodeTree

--- 231117/시험 통과 여부 확인하기/verify-test-passed.js
+++ 231117/시험 통과 여부 확인하기/verify-test-passed.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0).toString()
 const score = Number(input)
 
 
-if(score > 80){
+if(score >= 80){
     console.log('pass')
 }else{
     console.log(`${80-score} more score`)
