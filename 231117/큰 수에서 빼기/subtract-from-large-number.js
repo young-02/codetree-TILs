4f7f17c09bf221b1f7f87fc42c0f56cbda807c93
young <75434044+young-02@users.noreply.github.com>
2023-11-17 11:48:53 +0900

    Title: 큰 수에서 빼기, Time: 201ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231117/큰 수에서 빼기/subtract-from-large-number.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().split(' ')
+const a = Number(input[0])
+const b = Number(input[1])
+
+
+if(a > b) {
+    return
+}else{
+    console.log(b-a)
+}
