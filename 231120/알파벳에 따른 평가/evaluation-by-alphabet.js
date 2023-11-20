814eb0254211de150655d5f16cf4554ae57cfa83
young <75434044+young-02@users.noreply.github.com>
2023-11-20 14:34:32 +0900

    Title: 알파벳에 따른 평가, Time: 214ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 231120/알파벳에 따른 평가/evaluation-by-alphabet.js
+++ 231120/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -4,7 +4,7 @@ const input = fs.readFileSync(0).toString()
 if(input === 'S'){
     console.log('Superior')
 }else if(input === "A"){
-    console.log('Exellent')
+    console.log('Excellent')
 }else if(input=== "B"){
     console.log('Good')
 }else if(input === 'C'){
