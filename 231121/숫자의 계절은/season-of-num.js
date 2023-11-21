0fcc5d5832ad90005eba4e2456b6c1ffce73f950
young <75434044+young-02@users.noreply.github.com>
2023-11-21 15:47:55 +0900

    Title: 숫자의 계절은, Time: 230ms, Memory: 13MB, Status: Passed - CodeTree

--- 231121/숫자의 계절은/season-of-num.js
+++ 231121/숫자의 계절은/season-of-num.js
@@ -7,7 +7,7 @@ if(month >=3 && month <= 5 ){
     console.log('Spring')
 }else if(month >=6 && month <= 8){
     console.log('Summer')
-}else if(month >=9 && month >= 11){
+}else if(month >= 9 && month <= 11){
      console.log('Fall')
 }else if(month <= 2 || month == 12 ){
     console.log('Winter')
