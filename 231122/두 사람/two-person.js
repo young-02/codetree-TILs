b1077ccd792cca7deb1dfbaa8ad8c040d82ed26e
young <75434044+young-02@users.noreply.github.com>
2023-11-22 11:07:56 +0900

    Title: 두 사람, Time: 239ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231122/두 사람/two-person.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().split('\n')
+const a = input[0].split(' ')
+const b = input[1].split(' ')
+
+const a_age = a[0], a_gender =a[1]
+const b_age = b[0], b_gender = b[1]
+
+//19세 이상 이면서 남자
+if(a_age >= 19 && a_gender === "M" || b_age >= 19 && b_gender === "M"){
+    console.log(1)
+}
