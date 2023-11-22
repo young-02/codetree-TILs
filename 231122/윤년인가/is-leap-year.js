6686551aef9dd25ec939c49157fa8d6702f2c468
young <75434044+young-02@users.noreply.github.com>
2023-11-22 11:18:14 +0900

    Title: 윤년인가, Time: 231ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231122/윤년인가/is-leap-year.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString()
+const y = Number(input)
+
+if((y % 4 === 0 || y % 100 === 0) && y % 400 === 0 ){
+    console.log('true')
+}else{
+    console.log('false')
+}
