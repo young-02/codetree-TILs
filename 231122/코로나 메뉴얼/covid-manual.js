063e9ca0547c932c2197917b32817a17bb950713
young <75434044+young-02@users.noreply.github.com>
2023-11-22 14:52:40 +0900

    Title: 코로나 메뉴얼, Time: 243ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 231122/코로나 메뉴얼/covid-manual.js
+++ 231122/코로나 메뉴얼/covid-manual.js
@@ -1,27 +1,46 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().split('\n')
 
-let A = 0, B = 0, C = 0, D = 0
+// let A = 0, B = 0, C = 0, D = 0
 
-for(let i = 0; i< input.length; i++){
-     const arr = input[i].split(' ')
-     const cold = arr[0]
-     const  temp = Number(arr[1])
+// for(let i = 0; i< input.length; i++){
+//      const arr = input[i].split(' ')
+//      const cold = arr[0]
+//      const  temp = Number(arr[1])
      
-    if(cold === 'Y' && temp >= 37){
-        A++
-    }else if(cold === 'N' && temp >= 37){
-        B++
-    }else if(cold === 'Y' && temp <= 37){
-        C++
-    }else{
-        D++
-    }
+//     if(cold === 'Y' && temp >= 37){
+//         A++
+//     }else if(cold === 'N' && temp >= 37){
+//         B++
+//     }else if(cold === 'Y' && temp <= 37){
+//         C++
+//     }else{
+//         D++
+//     }
    
-}
+// }
 
-if(A >= 2){
+// if(A >= 2){
+//         console.log('E')
+//     }else{
+//    console.log('N')
+// }
+
+
+const a = input[0].split(' ')
+const b = input[1].split(' ')
+const c = input[2].split(' ')
+
+const a_state = a[0] ,a_temp = Number(a[1])
+const b_state = b[0] ,b_temp = Number(b[1])
+const c_state = a[0] ,c_temp = Number(c[1])
+
+if(a_state === "Y" && a_temp >= 37){
+    if(b_state === "Y" && b_temp >= 37 || c_state === 'Y' && c_temp >= 37 ){
         console.log('E')
     }else{
-   console.log('N')
+        console.log('N')
+    }
+}else{
+    console.log('N')
 }
