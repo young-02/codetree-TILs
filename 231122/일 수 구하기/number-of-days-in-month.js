adb281ac74aa33b5b6190aca30ff0a0dfcbd4de7
young <75434044+young-02@users.noreply.github.com>
2023-11-22 12:20:05 +0900

    Title: 일 수 구하기, Time: 241ms, Memory: 13MB, Status: Passed - CodeTree

--- 231122/일 수 구하기/number-of-days-in-month.js
+++ 231122/일 수 구하기/number-of-days-in-month.js
@@ -4,10 +4,11 @@ const input = fs.readFileSync(0).toString()
 const month = Number(input)
 
 
-//1 3 5 7 9  11
+
 if(month === 2){
     console.log(28)
-}else if(month === 1 || month === 3 || month === 5 || month === 7 || month === 8|| month===10 || month ===12){
+// 7월 이하 홀수 이거나 7월이상 짝수 일경우
+}else if((month <= 7 && month %2 === 1) || (month >= 7 && month %2 === 0)){
     console.log(31)
 }else{
     console.log(30)
