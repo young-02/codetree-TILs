a2f1ac732f2294a939560e80b5c84a36a9a3e647
young <75434044+young-02@users.noreply.github.com>
2023-11-23 12:25:23 +0900

    Title: b부터 a까지 감소, Time: 225ms, Memory: 45MB, Status: Passed - CodeTree

---
+++ 231123/b부터 a까지 감소/dec-from-b-to-a.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().split(' ')
+const a = Number(input[0])
+const b = Number(input[1])
+
+let result = ''
+for(let i = b; i>=a; i--){
+    result += i + ' '
+}
+console.log(result)
