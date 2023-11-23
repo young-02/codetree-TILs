e44e69b42a93c3d9d7f83bd315396b89e96240f2
young <75434044+young-02@users.noreply.github.com>
2023-11-23 12:05:52 +0900

    Title: 정수 입력받아 배수 출력, Time: 246ms, Memory: 14MB, Status: Passed - CodeTree

--- 231123/정수 입력받아 배수 출력/print-multiple-of-input.js
+++ 231123/정수 입력받아 배수 출력/print-multiple-of-input.js
@@ -6,8 +6,9 @@ const n = Number(input)
 let result = ''
 
 let num = 0
+let len = Number.MAX_SAFE_INTEGER
 
-for(let i = n; i <= 100; i++){
+for(let i = n; i < 1000; i++){
     if(i%n === 0){
        num++
        if(num <= 5){
