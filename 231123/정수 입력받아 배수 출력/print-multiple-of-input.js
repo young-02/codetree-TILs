36525f3ad5e8643a681c967ee94c69c2f20717f7
young <75434044+young-02@users.noreply.github.com>
2023-11-23 12:00:56 +0900

    Title: 정수 입력받아 배수 출력, Time: 232ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231123/정수 입력받아 배수 출력/print-multiple-of-input.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const input =fs.readFileSync(0).toString()
+
+const n = Number(input)
+ 
+let result = ''
+let num = 0
+for(let i = n; i <= 100; i++){
+    if(i%n === 0){
+       num++
+       if(num <= 5){
+        result +=(i + ' ')
+       }
+    }
+}
+
+console.log(result)
