b98153d764d5468c43ce0167bedfdf4c9604c2a6
young <75434044+young-02@users.noreply.github.com>
2023-11-24 18:29:42 +0900

    Title: 100점 도달하기, Time: 206ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231124/100점 도달하기/reach-100-point.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString()
+const n = Number(input)
+
+let result = ''
+
+for(let i = n; i <= 100; i++){
+    if(i >= 90){
+        result += 'A' + ' '
+    }else if(i >= 80){
+        result += 'B' + ' '
+    }else if(i >= 70){
+        result += 'C' + ' '
+    }else if( i >= 60){
+        result += 'D' + ' '
+    }else{
+        result += 'F'
+    }
+}
+
+console.log(result)
