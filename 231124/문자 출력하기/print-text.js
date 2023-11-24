b1040300ad5862df05c2430f4f3f6a42183e72a3
young <75434044+young-02@users.noreply.github.com>
2023-11-24 14:21:24 +0900

    Title: 문자 출력하기, Time: 211ms, Memory: 13MB, Status: Passed - CodeTree

--- 231124/문자 출력하기/print-text.js
+++ 231124/문자 출력하기/print-text.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
-const input = fs.readFileSync(0).toString()
+const input = fs.readFileSync(0).toString().trim()
 
 let result = ''
 for(let i = 1; i <= 8; i++){
-    result +=input
+   result += input
 }
 
 console.log(result)
