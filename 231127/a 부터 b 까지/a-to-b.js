d5db5f81f48fe4bcacf6504382cb47ef4a747249
young <75434044+young-02@users.noreply.github.com>
2023-11-27 18:14:22 +0900

    Title: a 부터 b 까지, Time: 246ms, Memory: 13MB, Status: Passed - CodeTree

--- 231127/a 부터 b 까지/a-to-b.js
+++ 231127/a 부터 b 까지/a-to-b.js
@@ -1,32 +1,18 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().split(' ')
 
-const a = Number(input[0])
-const b = Number(input[1])
+let a = Number(input[0])
+let b = Number(input[1])
 
 let result = ''
-let num = 0
-for(let i = a; i<=b; i++){
 
-    if(i === a){
-        num = i
-        result += num + ' '
-    }
-  if(num % 2 === 0 ){
-     num = num+ 3 
-     if(num <=13){
-     result += num + ' '
-     }
-    
-  }else if(num % 2 === 1 && num <=13){
-    num = num*2
-       if(num <=13){
-     result += num + ' '
-     }
-    
+while(a <=b){
+  result += a + " ";
+  if( a % 2 === 1){
+    a *=2
+  }else{
+    a +=3
   }
-  
-
 }
 
 console.log(result)
