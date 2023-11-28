5d31768bf93964645d4a335aa19034f1ae441dd9
young <75434044+young-02@users.noreply.github.com>
2023-11-28 12:40:30 +0900

    Title: 등장횟수 확인, Time: 291ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 231128/등장횟수 확인/check-number-of-appearances.js
+++ 231128/등장횟수 확인/check-number-of-appearances.js
@@ -4,7 +4,9 @@ const input = fs.readFileSync(0).toString().split('\n')
 let cnt = 0
 
 for(let i = 0; i<=5; i++){
-    if(Number(input[i])%2 === 0){
+    const x = Number(input[i])
+    if(x %2 === 0){
+
         cnt++
     }
 }
