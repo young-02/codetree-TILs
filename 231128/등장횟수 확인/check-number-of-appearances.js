757e954e8200e0b24da930441ffad7c9396e4be4
young <75434044+young-02@users.noreply.github.com>
2023-11-28 12:36:30 +0900

    Title: 등장횟수 확인, Time: 239ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 231128/등장횟수 확인/check-number-of-appearances.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().split('\n')
+
+let cnt = 0
+
+for(let i = 0; i<=input.length-1; i++){
+
+    if(input[i]%2 === 0){
+        cnt++
+    }
+}
+
+console.log(cnt)
