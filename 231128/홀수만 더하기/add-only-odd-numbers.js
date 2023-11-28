8d2ac4acaf32950b80432f9e20dcb7c9fe3c1452
young <75434044+young-02@users.noreply.github.com>
2023-11-28 18:32:54 +0900

    Title: 홀수만 더하기, Time: 206ms, Memory: 12MB, Status: Wrong Answer - CodeTree

--- 231128/홀수만 더하기/add-only-odd-numbers.js
+++ 231128/홀수만 더하기/add-only-odd-numbers.js
@@ -1,18 +1,12 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split('\n')
-const n = input[0]
-const arr = input.shift()
-
-
-
+const n = Number(input[0])
 let sum = 0;
 
-for(let i = 0; i<= input.length-1; i++){
+for(let i = 0; i<= n; i++){
   
-  if(n % 2 === 1){
-      if(Number(input[i]) % 3 === 0){
+  if(n % 2 === 1 && Number(input[i]) % 3 === 0){
          sum+= Number(input[i])
-      }
   }
 }
 
