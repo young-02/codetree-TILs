e84e1616299369e6b6a39db1b9bda20284766f86
young <75434044+young-02@users.noreply.github.com>
2023-11-28 12:16:39 +0900

    Title: 청소당번 정하기, Time: 222ms, Memory: 13MB, Status: Passed - CodeTree

--- 231128/청소당번 정하기/cleaning-numbering.js
+++ 231128/청소당번 정하기/cleaning-numbering.js
@@ -7,22 +7,18 @@ let cnt2 = 0
 let cnt3 = 0
 
 for(let i = 1; i <= n; i++){
-    if(i %  2 === 0){
-        cnt++
-    }
-    if(i % 3 === 0){
-        cnt2++
-    }
+    
     if(i % 12 === 0){
         cnt3++
     }
-    if((i% 2  === 0 && i%3 === 0) || (i% 2  === 0 && i % 12 === 0)){
-        cnt--
+   
+    else if(i % 3 === 0){
+        cnt2++
     }
-    
-    if(i% 3  === 0 && i % 12 === 0){
-        cnt2--
+    else  if(i %  2 === 0){
+        cnt++
     }
+
 }
 
 console.log(cnt, cnt2, cnt3)
