00094212d02cb7fd4a0f1ef20309930ff689a6db
young <75434044+young-02@users.noreply.github.com>
2023-11-30 15:20:25 +0900

    Title: 완전수 판별기, Time: 225ms, Memory: 45MB, Status: Passed - CodeTree

--- 231130/완전수 판별기/perfect-number-discriminator.js
+++ 231130/완전수 판별기/perfect-number-discriminator.js
@@ -1,15 +1,14 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString()
 const n = Number(input)
-// 6
-// 1,2,3
+
 let sum = 0
 for(let i = 1; i<=n; i++){
-    sum+=i
-    if(sum === n){
-       return console.log('P')
-    }else if(sum >= n){
-       return  console.log('N')
-
+ if( n % i === 0){
+    if(i < n){
+        sum+=i
     }
+ }  
 }
+
+console.log( sum === n ? 'P':'N')
