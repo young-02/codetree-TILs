85a6ad43b2a6fd810cdac38a44c823a1012b1be2
young <75434044+young-02@users.noreply.github.com>
2023-12-11 17:58:10 +0900

    Title: 1까지 나누기, Time: 218ms, Memory: 13MB, Status: Passed - CodeTree

--- 231211/1까지 나누기/divide-by-1.js
+++ 231211/1까지 나누기/divide-by-1.js
@@ -2,11 +2,21 @@ const fs = require('fs')
 const input = fs.readFileSync(0).toString()
 const n = Number(input)
 
-let i = 0
-let result = n
-while(result >= 1){
-    i++
-    result = parseInt(result/i)
+// let i = 0
+// let result = n
+// while(result >= 1){
+//     i++
+//     result = parseInt(result/i)
+// }
+// console.log(i)
 
+let result = n
+let count = 0
+for(let i = 1; i<=n; i++){
+  result = parseInt(result/i)
+  count++
+   if(result <= 1){
+    break
+ }
 }
-console.log(i)
+  console.log(count)
