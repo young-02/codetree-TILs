3f2ab25dd219fa7e5cf1324a38f9c68325e3abdb
young <75434044+young-02@users.noreply.github.com>
2024-03-09 11:57:34 +0900

    Title: 몫 출력하기, Time: 237ms, Memory: 47MB, Status: Passed - CodeTree

--- 240309/몫 출력하기/print-share.js
+++ 240309/몫 출력하기/print-share.js
@@ -2,17 +2,20 @@ const fs = require('fs')
 let input = fs.readFileSync(0).toString().trim().split('\n')
 
 let idx = 0;
+let cnt = 0
 
 while(true){
     let n = Number(input[idx])
     idx++
-    
+    let result = n
     if(n % 2 === 0){
-        console.log(n/2)
+        result = parseInt(n/2)
+        cnt++
     }else{
         continue;
     }
-    if(idx > 4){
+    console.log(result)
+    if(cnt >=3 ){
       break
     }
 }
