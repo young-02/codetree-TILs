c415e72c252b0bf137acd40f1931fad83b3779d4
young <75434044+young-02@users.noreply.github.com>
2024-03-10 11:48:14 +0900

    Title: 합성수 판별기, Time: 244ms, Memory: 14MB, Status: Passed - CodeTree

--- 240310/합성수 판별기/factorization-discriminator.js
+++ 240310/합성수 판별기/factorization-discriminator.js
@@ -2,18 +2,9 @@ const fs = require('fs')
 let n = Number(fs.readFileSync(0).toString())
 
 
-let idx = 1;
-
-while(n >= idx){
-    if(n %  idx === 0){
-         idx++
-         if( idx >=3){
-            console.log('C')
-            break
-         }
-    }else{
-        console.log('N')
-        break;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return console.log('C');
+        }
     }
-    
-}
+return console.log('N')
